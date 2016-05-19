3667da8c95109b6052b407feeb37565a9f664a59
Samuel Beek <ik@samuelbeek.com>
2016-05-19 23:06:26 +0200

    Create podcast model

---
+++ jeanmichel/Models/Podcast.swift
@@ -0,0 +1,15 @@
+//
+//  Podcast.swift
+//  jeanmichel
+//
+//  Created by Samuel Beek on 19/05/16.
+//  Copyright © 2016 Samuel Beek. All rights reserved.
+//
+
+import Foundation
+
+struct Podcast {
+    let title : String
+    let audioUrl : NSURL
+    let imageUrl : NSURL
+}
