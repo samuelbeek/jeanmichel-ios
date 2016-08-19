5323e713725d83e349e1f513af10d31ab71a8908
Samuel Beek <ik@samuelbeek.com>
2016-08-19 18:55:41 +0200

    Update buildnumbers

---
+++ Scripts/update_build_number.sh
@@ -0,0 +1,8 @@
+branch=${1:-'master'}
+buildNumber=$(expr $(git rev-list $branch --count) - $(git rev-list HEAD..$branch --count))
+echo "Updating build number to $buildNumber using branch '$branch'."
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"
+if [ -f "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist" ]; then
+  /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}.dSYM/Contents/Info.plist"
+fi
+view
